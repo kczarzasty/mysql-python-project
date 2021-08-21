be24f0c5d81842c99d47d8b51a2bdc47634b3ca0
Kevin Czarzasty <82130684+kczarzasty@users.noreply.github.com>
2021-08-21 18:06:59 -0400

    Update database.py

--- database.py
+++ database.py
@@ -16,7 +16,7 @@ my_cursor.execute("SHOW DATABASES")
 for db in my_cursor:
     print(db[0])
 
-Create Table
+#Create Table
 my_cursor.execute("CREATE TABLE users (name VARCHAR(255), email VARCHAR(255), age INTEGER(10), user_id INTEGER AUTO_INCREMENT PRIMARY KEY)")
 my_cursor.execute("SHOW TABLES")
 for table in my_cursor:
